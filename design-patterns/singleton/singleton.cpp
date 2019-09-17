6ff4c878d992c51908bd3a82a284489980aca0e8
Olin <cuijianjoy@126.com>
2019-09-17 22:53:15 +0800

    [design-patterns] singleton
    Signed-off-by: Olin <cuijianjoy@126.com>

---
+++ design-patterns/singleton/singleton.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+class Log
+{
+public:
+    static Log* GetInstance()
+    {
+        static Log *instance = nullptr;
+        if(!instance) {
+            instance = new Log();
+        }
+        return instance;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Log *logInstance1 = Log::GetInstance();
+    Log *logInstance2 = Log::GetInstance();
+    cout<<"log1 address:"<<logInstance1<<endl;
+    cout<<"log2 address:"<<logInstance2<<endl;
+
+    delete logInstance1;
+    delete logInstance2;
+    return 0;
+}
